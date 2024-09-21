35de22575ae5489c533d106e9502b26b20dbba20
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-09-21 16:52:57 -0700

    created helper funtion

--- checklist.py
+++ checklist.py
@@ -1,31 +1,50 @@
 checklist = list()
 
-checklist.append('Blue')
-print(checklist)
-checklist.append('Orange')
-print(checklist)
 
-def create(item):
-    checklist.append(item)
+#Understanding CRUD
 
-def read(index):
+def create(item): #create
+    checklist.append({"item": item, "completed": False})
+
+def read(index): #read
     return checklist[index]
 
-checklist = ['Blue', 'Orange']
-checklist[1] = 'Cats'
-print(checklist)
+def update(index, item): #update
+    checklist[index]["item"] = item
 
-def update(index, item):
-    checklist[index] = item
-
-checklist = ['Blue', 'Cats']
-checklist.pop(1)
-print(checklist)
-
-def destroy(index):
+def destroy(index): #destroy
     checklist.pop(index)
 
-def test():
+def list_all_items(): #check list items loop
+    for check, entry in enumerate(checklist):
+        check_status = "√" if entry["completed"] else " "
+        print(f"{check} [{check_status}] {entry['item']}")
+
+def mark_completed(index):
+    if index < len(checklist):
+        checklist[index]["completed"] = True
+        print(f"Item '{checklist[index]['item']}' marked as completed.")
+    else:
+        print(f"Index {index} is out of range.")
+
+def select(function_code):
+    if function_code == "C":
+        input_item = user_input("Input Item:")
+        create(input_item)
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+        read(int(item_index))
+    elif function_code == "P":
+        list_all_items()
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+    
+
+def test(): #tester
     create('Purple Sox')
     create("Red Cloak")
 
@@ -33,9 +52,24 @@ def test():
     print(read(1))
 
     update(0, "purple socks")
+
     destroy(1)
 
     print(read(0))
-    print(read(1))
+    
+    list_all_items()
+
+    select("C")
+
+    list_all_items()
+
+    select("R")
+
+    list_all_items()
+
+    user_value = user_input("Please Enter a Value:")
+    print(user_value)
 
 test()
+
+
